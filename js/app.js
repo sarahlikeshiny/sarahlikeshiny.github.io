fa94d8ca58202900a62816f7b8857eb85d27790e
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-05-08 19:19:12 +0100

    finished basic structure, markup and styles

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 $(() => {
 
+  const $links = $('.nav-link');
+  const $window = $(window);
+  const $menu = $('.menu');
+  const $body = $('body');
+  const $social =$('.social');
+
+  $links.on('click', scrollToSection);
+  $menu.on('click', toggleMenu);
 
 
+  function toggleMenu() {
+    $('.dropdown').slideToggle();
+  }
 
+  function scrollToSection() {
+    const section = $(this).attr('href');
+    $body.animate( {
+      scrollTop: $(section).offset().top
+    }, 1000, () => {
+      if ($window.width() < 575) {
+        $('.dropdown').slideToggle();
+      }
+    });
+  }
 
-
-
+  $social.hover(
+    function() {
+      $(this).addClass('bounce');
+    }
+  );
 
 });
