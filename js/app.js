401f4ff783e0d41d8167b21775ed579301a7be5b
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-05-12 17:19:03 +0100

    changed out jQuery added animation on scroll

--- js/app.js
+++ js/app.js
@@ -1,34 +1,14 @@
 $(() => {
 
-  const $links = $('.nav-link');
-  const $window = $(window);
-  const $menu = $('.menu');
-  const $body = $('body');
-  const $social =$('.social');
+  const $links = $('.nav');
 
   $links.on('click', scrollToSection);
-  $menu.on('click', toggleMenu);
-
-
-  function toggleMenu() {
-    $('.dropdown').slideToggle();
-  }
 
   function scrollToSection() {
     const section = $(this).attr('href');
-    $body.animate( {
+    $('body').animate( {
       scrollTop: $(section).offset().top
-    }, 1000, () => {
-      if ($window.width() < 575) {
-        $('.dropdown').slideToggle();
-      }
-    });
+    }, 1000);
   }
-
-  $social.hover(
-    function() {
-      $(this).addClass('bounce');
-    }
-  );
 
 });
