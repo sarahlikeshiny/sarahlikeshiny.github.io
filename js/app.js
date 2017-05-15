21b6e3deb04f8e962b70daddfe0d7b7229f26be6
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-05-15 12:13:37 +0100

    formatting changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 $(() => {
 
   const $links = $('.nav');
+  const $header = $('header');
+  const $window = $(window);
+
 
   $links.on('click', scrollToSection);
+  $window.scroll(updateHeader).trigger('scroll');
+
 
   function scrollToSection() {
     const section = $(this).attr('href');
     $('body').animate( {
       scrollTop: $(section).offset().top
     }, 1000);
+  }
+
+  function updateHeader() {
+    const bottomOfHeader = $header.offset().top + $header.height();
+    const viewportHeight = $window.height();
+
+    if (bottomOfHeader >= viewportHeight) {
+      $header.addClass('opaque');
+    } else {
+      $header.removeClass('opaque');
+    }
   }
 
 });
